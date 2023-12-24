4f3b52b8499fe7f59308d39aca9f4ad9320a8c81
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2023-12-24 05:01:40 -0300

    fix: shuffle icon color

--- src/components/Icons/IconShuffle.tsx
+++ src/components/Icons/IconShuffle.tsx
@@ -2,5 +2,5 @@ import { IconProps } from "solid-icons";
 import { FaSolidShuffle } from "solid-icons/fa";
 
 export default function IconShuffle(props: IconProps) {
-    return <FaSolidShuffle {...props} />
+    return <FaSolidShuffle {...props} color="currentColor" />
 }
