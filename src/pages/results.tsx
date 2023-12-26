743728b280836f454c95675fc8e963bda3732214
KarlitosD <37454843+KarlitosD@users.noreply.github.com>
2023-12-26 01:43:21 -0300

    feat: add skeleton for search results

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,6 +1,6 @@
 import type { Song } from "~/types/playlist"
 import { For } from "solid-js"
-import { RouteSectionProps, createAsync } from "@solidjs/router"
+import { type RouteSectionProps, createAsync } from "@solidjs/router"
 
 import { searchResults } from "./results.data"
 import { usePlaylist } from "~/context/playlist"
@@ -13,7 +13,7 @@ export default function Results(props: RouteSectionProps) {
   return (
     <>
       <div class="flex flex-col gap-2 p-4">
-        <For each={songs()} fallback={<p>Loading...</p>}>
+        <For each={songs()} fallback={<ResultFallback />}>
           {result => <ItemResult result={result} onSelect={() => addSong(result)} />}
         </For>
       </div>
@@ -35,5 +35,25 @@ function ItemResult(props: ItemResultProps) {
         <p class="text-xs text-ellipsis">{props.result.artists.name} - {props.result.album} - {props.result.duration.label}</p>
       </div>
     </div>
+  )
+}
+
+function ItemResultSkeleton() {
+  return (
+    <div class="flex items-center gap-2 w-80 sm:w-96 cursor-pointer text-left text-slate-300 animate-pulse" role="status">
+      <div class={`aspect-square w-[60px] h-[60px] bg-zinc-700`}></div>
+      <div class="text-left flex flex-col gap-1">
+        <div class="h-4 w-20 bg-zinc-700 rounded-full"></div>
+        <div class="h-3 w-40 bg-zinc-700 rounded-full"></div>
+      </div>
+    </div>
+  )
+}
+
+function ResultFallback () {
+  return (
+    <For each={Array.from({ length: 15 }, (_, i) => i)}>
+      {ItemResultSkeleton}
+    </For>
   )
 }
