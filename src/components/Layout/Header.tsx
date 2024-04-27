b7465c67901402e13b0dde0b7038250185cdbb50
KarlitosD <carlosdcampos.04@gmail.com>
2024-04-27 20:32:49 -0300

    feat: embedded logo in the header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,7 @@
 import { useLocation, useIsRouting, action, redirect } from '@solidjs/router';
 import IconMagnifyingGlass from "~/components/Icons/IconMagnifyingGlass";
 import { searchResults } from '~/pages/results.data';
+import { LogoApp } from '~/components/Icons/LogoApp';
 
 const searchAction = action(async (formData: FormData) => { 
   const search = formData.get('search') as string
@@ -16,7 +17,9 @@ export function Header() {
     return (
       <>
         <nav class="container mx-auto flex items-center justify-around p-4 gap-10 text-gray-600">
-          <a href="/" class="hidden sm:block"><img src="/icon.svg" class="" alt="Logo" width={60} /></a>
+          <a href="/" class="hidden sm:block sm:w-16" >
+            <LogoApp />
+          </a>
           <form class="form-control ml-0 sm:ml-4 lg:ml-0" action={searchAction} method="post">
             <fieldset class="join w-full" disabled={isRouting()}>
               <input class="input input-bordered text-neutral-content w-72 sm:w-96 join-item" type="search" name="search" placeholder="Search" value={location?.query?.search || ""} />
