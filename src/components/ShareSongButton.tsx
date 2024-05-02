f96c8c7d4c0cf5c097d3bb96e7505078d3e907f5
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-02 10:50:32 -0300

    feat: for now only songs are shareable

--- src/components/ShareSongButton.tsx
+++ src/components/ShareSongButton.tsx
@@ -1,5 +1,6 @@
 import { Song } from "~/db/schema";
 import IconShare from "./Icons/IconShare";
+import { Show } from "solid-js";
 
 export function ShareSongButton(props: { song: Song }) {
 
@@ -17,8 +18,10 @@ export function ShareSongButton(props: { song: Song }) {
     }
 
     return ( 
-        <button onClick={handleShare} class="w-fit">
-            <IconShare size={20} />
-        </button>
+        <Show when={props.song.type === "song"} fallback={<div class="w-20" />}>
+            <button onClick={handleShare} class="w-fit">
+                <IconShare size={20} />
+            </button>
+        </Show>
     )
 }
