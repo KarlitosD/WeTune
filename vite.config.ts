3c5fc9c60366d2db4eed0267ab7a4af7a98363f4
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-02 11:11:17 -0300

    feat: add maskable icon for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -50,6 +50,12 @@ export default defineConfig({
             src: "/icon-512x512.png",
             sizes: "512x512",
             type: "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
           }
         ]
       },
