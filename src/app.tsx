b46e6526d5fbf33e4a62411dd0b8d1481e9863ca
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-02 04:02:23 -0300

    chore: add noindex for anti seo

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,19 @@
 import { RouteSectionProps } from '@solidjs/router';
+import { Meta, MetaProvider } from '@solidjs/meta';
+
 import { Header } from '~/components/Layout/Header';
 import { PlaylistProvider } from '~/context/playlist';
 
 export default function App (props: RouteSectionProps) {
   return (
     <>
-      <Header />
-      <PlaylistProvider>
-        {props.children}
-      </PlaylistProvider>
+    <MetaProvider>
+        <Meta name="robots" content="noindex, nofollow" />
+        <Header />
+        <PlaylistProvider>
+          {props.children}
+        </PlaylistProvider>
+      </MetaProvider>
     </>
   );
 };
