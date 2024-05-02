dc531e02b486b13d53e65a7bab5a22ad1c6593fc
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-02 02:53:50 -0300

    feat: add more pngs for pwas

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
 
 export default defineConfig({
-  resolve:{
-    alias:{
-      '~' : path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
     },
   },
   build: {
@@ -17,26 +17,42 @@ export default defineConfig({
     devtools({
       autoname: true
     }),
-    solidPlugin(), 
+    solidPlugin(),
     VitePWA({
       base: "/",
       includeAssets: ["icon.svg"],
       manifest: {
+        theme_color: "#38bdf8",
+        background_color: "#0f172a",
+        display: "minimal-ui",
+        scope: "/",
+        start_url: "/",
         name: "WeTune",
         short_name: "WeTune",
         description: "A music player for the web",
-        theme_color: "#38bdf8",
-        background_color: "#0f172a",
-        display: "standalone",
         icons: [
           {
-            src: "/icon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-            purpose: "any",
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
-        ],
-      }, 
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      },
     })
   ],
 });
