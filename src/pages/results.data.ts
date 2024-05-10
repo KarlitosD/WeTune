279576d0d22e04337fcd0d1045a6d00978083d72
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-10 16:29:54 -0300

    fix: filter null search results

--- src/pages/results.data.ts
+++ src/pages/results.data.ts
@@ -7,7 +7,10 @@ export const searchResults = cache(async (search: string) => {
       const res = await fetch(`/api/search?query=${search}`)
       if (!res.ok) throw new Error("An unexpected error has occurred")
       const result = await res.json() as { songs: Song[], videos: Song[] }
-      return result
+      return {
+        songs: result.songs.filter(Boolean),
+        videos: result.videos.filter(Boolean)
+      }
     } catch (error) {
       console.error(error)
       throw error
