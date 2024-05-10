1e0b0b60c26d5d51f6469c3c1c489aee7c4ec1b5
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-10 03:46:18 -0300

    feat: add icon

---
+++ src/components/Icons/IconOutlineArrowDownCircle.tsx
@@ -0,0 +1,6 @@
+import type { IconProps } from "solid-icons";
+import { FiArrowDownCircle } from "solid-icons/fi";
+
+export default function IconOutlineArrowDownCircle(props: IconProps) {
+    return <FiArrowDownCircle {...props} />
+}
