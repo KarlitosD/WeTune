ac638056c1e89626d0e09dedf1d8789001822ba5
KarlitosD <carlosdcampos.04@gmail.com>
2024-05-10 02:31:43 -0300

    feat: wip playlist dropdwn operations

--- src/pages/playlist/[playlistId].tsx
+++ src/pages/playlist/[playlistId].tsx
@@ -1,57 +1,67 @@
 import { RouteSectionProps } from "@solidjs/router";
 import { For, Show, createMemo } from "solid-js";
+import { Dropdown, DropdownItem } from "~/components/Dropdown";
 import { IconPause, IconPlay } from "~/components/Icons";
+import IconEllipsis from "~/components/Icons/IconEllipsis";
 import IconTrash from "~/components/Icons/IconTrash";
 import { SongItem } from "~/components/SongItem";
 import { usePlaylist } from "~/context/playlist";
+import { Playlist } from "~/db/schema";
 import { audioPlayerEvent } from "~/utils/events";
 
-export default function PlaylistPage (props: RouteSectionProps) {
-    const { playlists, actualPlaylist, playSong, removeSong } = usePlaylist()
+export default function PlaylistPage(props: RouteSectionProps) {
+    const { playlists, playSong, removeSong } = usePlaylist()
     const playlist = createMemo(() => playlists().find(p => p.id === props.params.playlistId))
 
-    const isActualPlaylist = () => actualPlaylist().id === playlist().id
+    return (
+        <section class="h-full">
+            <Show when={playlist()?.id}>
+                <PlaylistHeader playlist={playlist()} />
+                <article class="flex flex-col my-4 px-4 gap-2 max-h-[700px] sm:max-h-[750px] h-[85%] overflow-y-scroll scrollbar scrollbar-track-base-100 scrollbar-thumb-primary">
+                    <For each={playlist().songs}>{ song =>
+                        <SongItem song={song} onSelect={() => playSong(song, playlist().id)}>
+                            <DropdownItem>
+                                <button class="text-error p-1 flex items-center gap-3" onClick={() => removeSong(song, playlist().id)}>
+                                    <IconTrash size={14} />
+                                    <span>Eliminar</span>
+                                </button>
+                            </DropdownItem>
+                        </SongItem>
+                    }</For>
+                </article>
+            </Show>
+        </section>
+    )
+}
+
+function PlaylistHeader(props: { playlist: Playlist }) {
+    const { actualPlaylist, playSong } = usePlaylist()
+    const isActualPlaylist = () => actualPlaylist().id === props.playlist.id
 
     const handlePlayPlaylist = () => {
-        if(!isActualPlaylist()) {
-            const firstSong = playlist().songs[0]
-            playSong(firstSong, playlist().id)
+        if (!isActualPlaylist()) {
+            const firstSong = props.playlist.songs[0]
+            playSong(firstSong, props.playlist.id)
         } else {
             audioPlayerEvent.emit("togglePlay",)
         }
     }
 
     return (
-        <section class="h-full">
-            <Show when={playlist()?.id}>
-                <header class="flex justify-between items-center py-3 px-6 border-b border-b-gray-500">
-                    <h1 class="text-xl text-base-content">{playlist().title}</h1>
-                    <button class="btn btn-primary rounded-full" onClick={handlePlayPlaylist}>
-                        <Show when={isActualPlaylist()} fallback={<IconPlay />}>
-                            <IconPause />
-                        </Show>
-                    </button>
-                </header>
-                <article class="flex flex-col my-4 px-4 gap-2 max-h-[700px] sm:max-h-[750px] h-[85%] overflow-y-scroll scrollbar scrollbar-track-base-100 scrollbar-thumb-primary">
-                    <For each={playlist().songs}>
-                        {song =>
-                            <SongItem song={song} onSelect={() => playSong(song, playlist().id)}>
-                                <li>
-                                    <div>
-                                        <button
-                                            onClick={() => removeSong(song, playlist().id)}
-                                            class="text-error p-1 flex items-center gap-3"
-                                        >
-                                            <IconTrash size={14} />
-                                            <span>Eliminar</span>
-                                        </button>
-                                    </div>
-                                </li>
-                            </SongItem>
-                        }
-                    </For>
-                </article>
-            </Show>
-        </section>
+        <header class="flex justify-between items-center py-3 px-6 border-b border-b-gray-500">
+            <div class="flex items-end gap-3">
+                <h1 class="text-xl text-base-content">{props.playlist.title}</h1>
+                <Dropdown class="text-white" summary={<IconEllipsis />}>
+                    <DropdownItem>
+                        <button class="p-1 flex">Test</button>
+                    </DropdownItem>
+                </Dropdown>
+            </div>
+            <button class="btn btn-primary rounded-full" onClick={handlePlayPlaylist}>
+                <Show when={isActualPlaylist()} fallback={<IconPlay />}>
+                    <IconPause />
+                </Show>
+            </button>
+        </header>
     )
 }
