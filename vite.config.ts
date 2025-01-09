3cc9e4431261e7f1dd8ee0a708959c344ed4d268
KarlitosD <carlosdcampos.04@gmail.com>
2025-01-08 23:17:41 -0300

    feat: add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import devtools from '@solid-devtools/transform'
 import solidPlugin from 'vite-plugin-solid';
-import { VitePWA } from 'vite-plugin-pwa'
 import path from "path"
+
+const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
   resolve: {
@@ -10,77 +11,29 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src')
     },
   },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
-    target: 'esnext',
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
   plugins: [
     devtools({
       autoname: true
     }),
     solidPlugin(),
-    VitePWA({
-      strategies: "generateSW",
-      registerType: "autoUpdate",
-      base: "/",
-      includeAssets: ["icon.svg"],
-      manifest: {
-        theme_color: "#38bdf8",
-        background_color: "#0f172a",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        name: "WeTune",
-        short_name: "WeTune",
-        description: "A music player for the web",
-        icons: [
-          // {
-          //   src: "/icon-192x192.png",
-          //   sizes: "192x192",
-          //   type: "image/png"
-          // },
-          // {
-          //   src: "/icon-256x256.png",
-          //   sizes: "256x256",
-          //   type: "image/png"
-          // },
-          // {
-          //   src: "/icon-384x384.png",
-          //   sizes: "384x384",
-          //   type: "image/png"
-          // },
-          // {
-          //   src: "/icon-512x512.png",
-          //   sizes: "512x512",
-          //   type: "image/png"
-          // },
-          {
-            "src": "maskable-icon-512x512",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "maskable-icon-192x192",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
-        ]
-      },
-      workbox: {
-        // runtimeCaching: [{
-        //   urlPattern: new RegExp('^https://wsrv\\.nl/\\?url=https%3A%2F%2Fi\\.ytimg\\.com%2Fvi%2F.*%2Fmqdefault\\.jpg'),
-        //   handler: 'NetworkFirst',
-        //   options: {
-        //     cacheName: 'yt-thumbnails',
-        //     expiration: {
-        //       maxEntries: 200,
-        //       maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        //       purgeOnQuotaError: true,
-        //     },
-        //   }
-        // }]
-      }
-    })
   ],
+
+  server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+  }
 });
