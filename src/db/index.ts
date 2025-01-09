ee87d7b365af49e36da2f84a73d38fe20de4d6b3
KarlitosD <carlosdcampos.04@gmail.com>
2025-01-08 22:42:07 -0300

    chore update version of signaldb

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,17 @@
-import { Collection, createLocalStorageAdapter } from 'signaldb';
-import solidReactivityAdapter from 'signaldb-plugin-solid';
+import { Collection } from "@signaldb/core"
+import createIndexedDBAdapter from '@signaldb/indexeddb';
+import solidReactivityAdapter from '@signaldb/solid';
 import { Playlist } from './schema';
 import { PLAYLISTS } from '~/consts';
 
 export const playlistCollection = new Collection<Playlist>({
   reactivity: solidReactivityAdapter,
-  persistence: createLocalStorageAdapter("playlist")
+  persistence: createIndexedDBAdapter("playlist")
 });
 
-await new Promise(res => playlistCollection.on("persistence.init", () => res(null)))
+// await new Promise(res => playlistCollection.on("persistence.init", () => res(null)))
 
-if(playlistCollection.find({}).fetch().length === 0){
+if(playlistCollection.find({}, { reactive: false }).fetch().length === 0){
     playlistCollection.insert({ id: PLAYLISTS.HISTORY, title: "Recents", songs: [] })
 }
 
