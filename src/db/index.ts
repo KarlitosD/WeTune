652149d56b5e52065b7e680c11c37d4150d4d80d
KarlitosD <carlosdcampos.04@gmail.com>
2025-02-19 00:40:43 -0300

    fix: enable persistence initialization for playlist collection

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,7 +9,7 @@ export const playlistCollection = new Collection<Playlist>({
   persistence: createIndexedDBAdapter("playlist")
 });
 
-// await new Promise(res => playlistCollection.on("persistence.init", () => res(null)))
+await new Promise(res => playlistCollection.on("persistence.init", () => res(null)))
 
 if(playlistCollection.find({}, { reactive: false }).fetch().length === 0){
     playlistCollection.insert({ id: PLAYLISTS.HISTORY, title: "Recents", songs: [] })
